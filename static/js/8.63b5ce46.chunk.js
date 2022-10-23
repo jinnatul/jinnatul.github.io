26d0357517b997eb122edf0042a46e050f6b4370
jinnatul <31995155+jinnatul@users.noreply.github.com>
2022-10-23 10:02:01 +0000

    Deploying to gh-pages from  @ 17706488925db1bc681985dca52962d502f101e5 🚀

---
+++ static/js/8.63b5ce46.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpjinnatul.github.io"]=this["webpackJsonpjinnatul.github.io"]||[]).push([[8],{86:function(t,i,n){"use strict";n.r(i);n(0),n(88);var e=n(1);i.default=function(t){var i=t.title;return Object(e.jsx)("div",{className:"header",children:i})}}}]);
+//# sourceMappingURL=8.63b5ce46.chunk.js.map
